00e85d77c3e4739b1cddf60b074eba0f91cd9058
Frank Landry <FLandry@vtc.vsc.edu>
2015-03-28 18:00:50 -0400

    Created nav.php

---
+++ nav.php
@@ -0,0 +1,20 @@
+<!-- Authors: David Conde, Wellington Newton, Kyle Justice, Frank Landry
+	 Version: 1.0
+	 Date: 02/15/2015 -->
+<!-- Set up the navigation and css that applies to it-->
+<link rel="stylesheet" type="text/css" href="navCSS.css"/>
+<div id="line" onmousedown="event.preventDefault()" >
+	<div id="nav" onmousedown="event.preventDefault()" align="center">
+		<a href="home.php">HOME</a>
+		<a href="faq.php">FAQ</a>
+		<a href="courses.php">COURSES</a>
+		<a href="product.php">PRODUCT</a>
+		<a href="contact.php">CONTACT US</a>
+		<a href="logout.php">LOGOUT</a>
+	</div>
+</div>
+
+
+
+
+
