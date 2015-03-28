77d44731f101cce93533bdcf18b0e7059ae046a4
Frank Landry <FLandry@vtc.vsc.edu>
2015-03-28 17:58:22 -0400

    Created footer.php

---
+++ footer.php
@@ -0,0 +1,39 @@
+<!-- Authors: Kyle Justice, David Conde, Wellington Newton, Frank Landry
+	 Version: 1.0
+	 Date: 02/20/2015 -->
+<!-- This is the footer code -->
+<link rel="stylesheet" type="text/css" href="footerCSS.css"/>
+<!-- Set up the divisions -->
+	<div id="footer" >
+		<center>
+		<br />
+		<p>
+		<!-- Set up the contact information including media links -->
+		<span style="color: #000000;font-weight: bold; text-decoration: none; "> Vermont Technical College </span></br> 
+		PO Box 500</br>
+		124 Admin Drive</br>
+		Randolph Center, VT 05061</br>
+		<span><a href="http://www.vtc.edu/admissions-aid/request-information">request info</a> | </span>
+		<span itemprop="telephone"><a href="tel:802-728-1000">(802) 728-1000</a></span></br>
+		</br>
+		</center>
+		<span style="color: #2CD142;font-weight: bold; text-decoration: none; display:block;text-align: left; padding-left: 10px;">
+		&copy; 2014 VERMONT TECHNICAL COLLEGE
+		</span>
+		<span style="color: #2CD142;font-weight: bold; text-decoration: none; display:block;text-align: right;">
+		<a href="https://www.facebook.com/pages/Vermont-Tech/66216087656" target="_blank" style="padding:0px;">
+			<img src="iconSocial/fb.png" onmousedown="event.preventDefault()" oncontextmenu="return false;" width="45" height="45">
+		</a>
+		<a href="http://twitter.com/VTTech" target="_blank" style="padding:0px;">
+			<img src="iconSocial/twitter.png" onmousedown="event.preventDefault()" oncontextmenu="return false;" width="45" height="45">
+		</a> 		
+		<a href="http://instagram.com/vttech" target="_blank"style="padding:0px;">
+			<img src="iconSocial/instagram.png" style="padding-right: 0px; padding-left: 0px;" onmousedown="event.preventDefault()" oncontextmenu="return false;" width="45" height="45">
+		</a> 
+		<a href="http://www.youtube.com/user/VermontTechCollege" target="_blank" style="padding-left: 0px;padding-top: 0px;padding-bottom: 0px;">
+			<img src="iconSocial/youtube.png" onmousedown="event.preventDefault()" oncontextmenu="return false;" width="45" height="45">
+		</a> 
+		</span>			
+		</p>
+<!-- FOOTER END -->
+	</div>
