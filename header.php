443a038c92714246806a80fa3c41dff433021b56
Frank Landry <FLandry@vtc.vsc.edu>
2015-03-28 18:00:00 -0400

    Created header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<!-- Authors: David Conde, Wellington Newton, Kyle Justice, Frank Landry
+	 Version: 1.0
+	 Date: 02/15/2015-->
+<h1 onmousedown="event.preventDefault()" oncontextmenu="return false;" >
+	VERMONT TECH
+	<!--	image background	-->
+</h1>
