2169101aab2750236612d8c78f3a32d28d3164c7
Frank Landry <FLandry@vtc.vsc.edu>
2015-03-28 17:46:15 -0400

    Update and rename courses.html to courses.php
    Added full functionality to the page.

---
+++ courses.php
@@ -0,0 +1,51 @@
+<?php 
+// Authors: Wellington Newton, David Conde, Kyle Justice, Frank Landry
+// Version: 1.0
+// Date: 02/14/2015
+// Set up the session
+session_start();
+?>
+<!-- Set up the web page -->
+<html lang="en"> 
+<title> Vermont Tech | Software Downloads </title>
+<!-- Set up the css -->
+<link rel="stylesheet" type="text/css" href="contactCSS.css"/>
+<!-- Set up the header and navigation -->
+<?php include 'header.php'; ?>
+<?php include 'nav.php'; ?>
+<!-- Set up the divisions -->
+<div class="container">
+<!-- CONTAINER START -->
+	<div class="content">
+	<!-- BODY START -->
+	<center>
+	<!-- List all the courses -->
+		<div id="title1" onmousedown="event.preventDefault()" oncontextmenu="return false;">
+			CIS Courses
+		</div>
+	Question?
+	<br />
+	Answer...
+	</center>
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<!-- BODY ENDS -->
+	</div>
+<!-- CONTENTS END -->
+</div>
+<!-- Set up the footer -->
+<img class="zindex" src="footer.png">
+<!-- FOOTER -->
+<?php include 'footer.php'; ?>	
+</html>
+
